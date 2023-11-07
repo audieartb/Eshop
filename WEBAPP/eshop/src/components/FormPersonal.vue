0310abb84b0f95ef2c7badf2e1bbff0d3f6c1f57
Audie Artavia <audiea@google.com>
2023-11-07 17:07:52 +0100

    form is responsive and validates data

--- WEBAPP/eshop/src/components/FormPersonal.vue
+++ WEBAPP/eshop/src/components/FormPersonal.vue
@@ -4,7 +4,7 @@ import { useCartStore } from '@/stores/cart'
 import { storeToRefs } from 'pinia'
 
 import { useVuelidate } from '@vuelidate/core'
-import { email, required } from '@vuelidate/validators'
+import { email, required, numeric } from '@vuelidate/validators'
 
 const store = useCartStore()
 const { carts } = storeToRefs(store)
@@ -34,7 +34,7 @@ const rules = {
   country: { required },
   city: { required },
   address: { required },
-  zip: { required },
+  zip: { required},
   delivery: { required }
 }
 
@@ -49,74 +49,129 @@ function clear() {
 </script>
 
 <template>
-    <div>
-        <form action="">
-        <v-text-field
-          v-model="state.name"
-          @input="v$.name.$touch"
-          @blur="v$.name.$touch"
-          label="First Name"
-          :error-messages="v$.name.$errors.map(e => e.$message)"
-          required
-        ></v-text-field>
-        <v-text-field
-          v-model="state.lastname"
-          @input="v$.lastname.$touch"
-          @blur="v$.lastname.$touch"
-          label="Last Name"
-          :error-messages="v$.lastname.$errors.map(e => e.$message)"
-          required
-        ></v-text-field>
-        <v-text-field
-          v-model="state.email"
-          @input="v$.email.$touch"
-          @blur="v$.email.$touch"
-          label="Email"
-          :error-messages="v$.email.$errors.map(e => e.$message)"
-          required
-        ></v-text-field>
-        <v-text-field
+  <div>
+    <v-container>
+        <v-row class="justify-center">
+            <div class="text-h6 " >Fill in to continue with payment</div>
+        </v-row>
+      <v-row>
+        <v-col class="v-col-lg-6 v-col-sm-12">
+          <v-text-field
+            class="row-field"
+            variant="outlined"
+            v-model="state.name"
+            @input="v$.name.$touch"
+            @blur="v$.name.$touch"
+            label="First Name"
+            :error-messages="v$.name.$errors.map((e) => e.$message)"
+            required
+          ></v-text-field>
+        </v-col>
+        <v-col class="v-col-lg-6 v-col-sm-12">
+          <v-text-field
+            class="row-field"
+            variant="outlined"
+            v-model="state.lastname"
+            @input="v$.lastname.$touch"
+            @blur="v$.lastname.$touch"
+            label="Last Name"
+            :error-messages="v$.lastname.$errors.map((e) => e.$message)"
+            required
+          ></v-text-field>
+        </v-col>
+      </v-row>
+
+      <v-row>
+        <v-col class="v-col-lg-6 v-col-sm-12">
+          <v-text-field
+            id="email-field"
+            variant="outlined"
+            v-model="state.email"
+            @input="v$.email.$touch"
+            @blur="v$.email.$touch"
+            label="Email"
+            :error-messages="v$.email.$errors.map((e) => e.$message)"
+            required
+          ></v-text-field>
+        </v-col>
+      </v-row>
+      
+      <v-row>
+        <v-col class="v-col-lg-6 v-col-sm-12">
+            <v-text-field
+          class="row-field"
+          variant="outlined"
           v-model="state.country"
           @input="v$.country.$touch"
           @blur="v$.country.$touch"
           label="Country"
-          :error-messages="v$.country.$errors.map(e => e.$message)"
+          :error-messages="v$.country.$errors.map((e) => e.$message)"
           required
         ></v-text-field>
+        </v-col>
+       <v-col class="v-col-lg-6 v-col-sm-12">
         <v-text-field
+          class="row-field"
+          variant="outlined"
           v-model="state.city"
           @input="v$.city.$touch"
           @blur="v$.city.$touch"
           label="City"
-          :error-messages="v$.city.$errors.map(e => e.$message)"
+          :error-messages="v$.city.$errors.map((e) => e.$message)"
           required
         ></v-text-field>
-        <v-text-field
-          v-model="state.address"
-          @input="v$.address.$touch"
-          @blur="v$.address.$touch"
-          label="Address"
-          :error-messages="v$.address.$errors.map(e => e.$message)"
-          required
-        ></v-text-field>
-        <v-text-field
-          v-model="state.zip"
-          @input="v$.zip.$touch"
-          @blur="v$.zip.$touch"
-          label="Zip Code"
-          :error-messages="v$.zip.$errors.map(e => e.$message)"
-          required
-        ></v-text-field>
-        <v-select
-          v-model="state.delivery"
-          :items="deliveryOpts"
-          @input="v$.delivery.$touch"
-          @blur="v$.delivery.$touch"
-          label="Delivery Type"
-          :error-messages="v$.delivery.$errors.map(e => e.$message)"
-          required
-        ></v-select>
-        <v-btn class="me-4">Submit</v-btn>
-      </form>
-    </div>
+       </v-col>
+      </v-row>
+
+      <v-row>
+        <v-col class="v-col-lg-6 v-col-sm-12">
+          <v-text-field
+            variant="outlined"
+            v-model="state.address"
+            @input="v$.address.$touch"
+            @blur="v$.address.$touch"
+            label="Address"
+            :error-messages="v$.address.$errors.map((e) => e.$message)"
+            required
+          ></v-text-field>
+        </v-col>
+      </v-row>
+
+      <v-row>
+        <v-col class="v-col-lg-6 v-col-sm-12">
+          <v-text-field
+            class="row-field"
+            variant="outlined"
+            v-model="state.zip"
+            @input="v$.zip.$touch"
+            @blur="v$.zip.$touch"
+            label="Zip Code"
+            :error-messages="v$.zip.$errors.map((e) => e.$message)"
+            required
+          ></v-text-field>
+        </v-col>
+        <v-col class="v-col-lg-6 v-col-sm-12">
+          <v-select
+            variant="outlined"
+            class="row-field"
+            v-model="state.delivery"
+            :items="deliveryOpts"
+            @input="v$.delivery.$touch"
+            @blur="v$.delivery.$touch"
+            label="Delivery Type"
+            :error-messages="v$.delivery.$errors.map((e) => e.$message)"
+            required
+          ></v-select>
+        </v-col>
+      </v-row>
+      <v-btn class="me-4">Submit</v-btn>
+    </v-container>
+  </div>
 </template>
+
+<style scoped>
+.row-field {
+  margin: 0;
+}
+
+</style>
--- WEBAPP/eshop/src/views/CartView.vue
+++ WEBAPP/eshop/src/views/CartView.vue
@@ -10,20 +10,20 @@ const { currentCart } = storeToRefs(store)
 </script>
 
 <template>
-  <div class="d-flex">
+  <div class="d-flex justify-space-between">
     <template v-if="carts[currentCart]">
-      <div class="d-flex align-center flex-column">
+      <div class="d-flex align-start flex-column w-50">
         <v-card
           v-for="(item, idx) in carts[currentCart].items"
           width="400"
           :title="item.barcode"
           subtitle="This is a subtitle"
           text="This is content"
-          class="m-3"
+          class="mb-5"
         ></v-card>
       </div>
     </template>
-    <div>
+    <div class="w-50">
     <FormPersonal></FormPersonal>
      
     </div>
